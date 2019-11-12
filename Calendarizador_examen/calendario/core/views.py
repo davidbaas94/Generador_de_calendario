ee0d2d904776cbeaca2f6271cbc73218ea72426e
davidbaas94 <47534130+davidbaas94@users.noreply.github.com>
2019-11-12 14:05:25 -0600

    Update views.py

--- Calendarizador_examen/calendario/core/views.py
+++ Calendarizador_examen/calendario/core/views.py
@@ -1,15 +1,52 @@
 from django.shortcuts import render, HttpResponse
+import requests
+from django.views.generic.base import TemplateView
+
 
 # Create your views here.
+class HomePageView(TemplateView):
+    #return render(request,'core/home.html')
+    template_name = "core/home.html"
 
-def grupos(request):
-    return render(request, "core/grupos.html")
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
 
-def asignatura(request):
-    return render(request, "core/asignatura.html")
+class GrupoPageView(TemplateView):
+    """ if request.method == 'GET':
+            
+            idNombre = ""
+            idCuatri = ""
 
-def aula(request):
-    return render(request, "core/aula.html")
+            idNombre = request.GET["idNombre"]
+            idCuatri = request.GET["idCuatri"]
+            
+            grupos = [idCuatri]
+            
+    # return render(request,'core/grupos.html')"""
 
-def calendar(request):
-    return render(request, "core/calendario.html")
+    template_name = "core/grupos.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+   
+
+class AsignaturaPageView(TemplateView):
+    #return render(request,'core/asignatura.html')
+    template_name = "core/asignatura.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+class AulaPageView(TemplateView):
+    #return render(request,'core/aula.html')
+    template_name = "core/aula.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+class CalendarioPageView(TemplateView):
+    #return render(request,'core/calendario.html')
+    template_name = "core/calendario.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
